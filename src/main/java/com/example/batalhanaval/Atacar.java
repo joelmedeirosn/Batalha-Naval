4026f62e95ffae0bcbc153e34682e77fe2cde61f
joelmedeirosn <jmn1@poli.br>
2022-12-07 18:40:19 -0300

    'teste'

--- src/main/java/com/example/batalhanaval/Atacar.java
+++ src/main/java/com/example/batalhanaval/Atacar.java
@@ -2,7 +2,7 @@ package com.example.batalhanaval;
 
 public class Atacar {
     private int[] atirar;
-    private int[][] cruz;
+    private int[][]cruz;
 
     private String Tiro;
     private String Agua;
